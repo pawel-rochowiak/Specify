803d2f1c0e1bdf51e1dd04360ad6a74a22b3e42d
Paweł Rochowiak <pawelrochowiak@gmail.com>
2022-11-21 00:16:49 +0100

    Seting up autenthication (Firebase) for creating new user

--- src/components/forms/NewUserForm.js
+++ src/components/forms/NewUserForm.js
@@ -1,26 +1,79 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Modal from "../../UI/Modal";
 import PasswordIcon from "../icons/PasswordIcon";
 import CheckIcon from "../icons/CheckIcon";
 import EmailIcon from "../icons/EmailIcon";
 import Logo from "../../Assets/specify_logo.png";
 import classes from "./NewUserForm.module.css";
+import LoadingSpinner from "../../UI/LoadingSpinner";
 
 const NewUserForm = (props) => {
+  const nameInputRef = useRef();
+  const surnameInputRef = useRef();
+  const passwordInputRef = useRef();
+  const emailInputRef = useRef();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    const enteredName = nameInputRef.current.value;
+    const enteredSurname = surnameInputRef.current.value;
+
+    setIsLoading(true);
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCvhJIuwY1ms610WG-tDggIcuKbGrSEd5o",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+        //...
+      } else {
+        return res.json().then((data) => {
+          let errorMessage = "Autenthication failed!";
+
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+          }
+          alert(errorMessage);
+        });
+      }
+    });
+  };
+
   const mainContent = (
     <div className={classes.container}>
       <div className={classes.logo}>
         <img src={Logo}></img>
       </div>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.formGroup}>
           <EmailIcon className={classes.icon} />
-          <input required type="text" id="name"></input>
+          <input required ref={nameInputRef} type="text" id="name"></input>
           <p className={classes.description}>Name</p>
         </div>
         <div className={classes.formGroup}>
           <EmailIcon className={classes.icon} />
-          <input required type="text" id="surname"></input>
+          <input
+            required
+            ref={surnameInputRef}
+            type="text"
+            id="surname"
+          ></input>
           <p className={classes.description}>Surname</p>
         </div>
         <div className={classes.formGroup}>
@@ -35,18 +88,23 @@ const NewUserForm = (props) => {
         </div>
         <div className={classes.formGroup}>
           <EmailIcon className={classes.icon} />
-          <input required type="text" id="confirmMail"></input>
+          <input ref={emailInputRef} type="text" id="confirmMail"></input>
           <p className={classes.description}>confirm e-mail</p>
         </div>
         <div className={classes.formGroup}>
           <CheckIcon className={classes.icon} />
-          <input required type="text" id="confirmPassword"></input>
+          <input
+            ref={passwordInputRef}
+            type="text"
+            id="confirmPassword"
+          ></input>
           <p className={classes.description}>confirm password</p>
         </div>
+        <div className={classes.buttons}>
+          {!isLoading && <button>submit</button>}
+          {isLoading && <LoadingSpinner />}
+        </div>
       </form>
-      <div className={classes.buttons}>
-        <button type="submit">submit</button>
-      </div>
     </div>
   );
 
--- src/components/icons/PasswordIcon.js
+++ src/components/icons/PasswordIcon.js
@@ -2,7 +2,7 @@ const PasswordIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      enable-background="new 0 0 36 36"
+      enableBackground="new 0 0 36 36"
       height="36px"
       viewBox="0 0 36 36"
       width="36px"
