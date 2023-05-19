10648066c0c60f1f08a3968346c41fbb35c0223a
Paweł Rochowiak <pawelrochowiak@gmail.com>
2023-05-19 14:34:46 +0200

    Projects Detail Page: Specification preview was improved

--- src/components/forms/PreviewPDF.js
+++ src/components/forms/PreviewPDF.js
@@ -3,11 +3,13 @@ import Modal from "../../UI/Modal";
 import { storage } from "../../firebase";
 import { ref, listAll, getDownloadURL } from "firebase/storage";
 import pdfMake from "pdfmake/build/pdfmake";
+//CSS module//
+
 import classes from "./PreviewPDF.module.css";
 
 const PreviewPDF = (props) => {
-  const [pdfUrlArr, setPdfUrlArr] = useState(null);
-  const [specNames, setSpecNames] = useState(null);
+  const [pdfUrlArr, setPdfUrlArr] = useState([]);
+  const [specNames, setSpecNames] = useState([]);
 
   useEffect(() => {
     pdfPreviewHandler();
@@ -17,33 +19,37 @@ const PreviewPDF = (props) => {
     const storageRef = ref(storage, `specifications/${props.project}`);
     try {
       const listResult = await listAll(storageRef);
-      const pdfUrls = await Promise.all(
-        listResult.items.map(async (item) => {
-          const pdfFile = await getDownloadURL(item);
-          return pdfFile;
-        })
-      );
-      const names = listResult.items.map(
-        (item) =>
-          item._location.path_
-            .toString()
-            .split("/")
-            .at(-1)
-            .split("-")[1]
-            .split(".")[0]
-      );
-      setPdfUrlArr(pdfUrls);
-      setSpecNames(names);
+      if (listResult.items.length !== 0) {
+        const pdfUrls = await Promise.all(
+          listResult.items.map(async (item) => {
+            const pdfFile = await getDownloadURL(item);
+            return pdfFile;
+          })
+        );
+        const names = listResult.items.map(
+          (item) =>
+            item._location.path_
+              .toString()
+              .split("/")
+              .at(-1)
+              .split("-")[1]
+              .split(".")[0]
+        );
+        setPdfUrlArr(pdfUrls);
+        setSpecNames(names);
+      }
     } catch (error) {
       console.error("Error downloading PDFs:", error);
     }
   };
 
-  console.log(specNames);
-
   return (
-    <Modal onClose={props.onClick} onExit={props.onExit}>
-      {specNames && (
+    <Modal
+      className={classes.modal}
+      onClose={props.onClick}
+      onExit={props.onExit}
+    >
+      {specNames.length !== 0 && (
         <div id="buttonContainer">
           {specNames.map((name, index) => (
             <button key={index}>{name}</button>
@@ -51,7 +57,15 @@ const PreviewPDF = (props) => {
         </div>
       )}
 
-      {pdfUrlArr && (
+      {specNames.length === 0 && (
+        <p className={classes.info_message}>
+          No PDF was saved to the database yet. In order to create a PDF press
+          <span> "PDF" </span> button at the bottom of the specification creator
+          page.
+        </p>
+      )}
+
+      {pdfUrlArr.length !== 0 && (
         <iframe
           src={pdfUrlArr[0]}
           frameBorder="0"
